34107bc01a08d2cea71bfb75b903ddcfefa8d327
ilgiz05 <zaydamustafaeva@gmail.com>
2021-11-14 17:07:20 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        double[] name = new double[] {23.23d,-90.23d, 13.65, -76.56d, 98.90d, 87.32d, -56.687d, -23.45d, -34.98d, -76.985d, 6859.6895d, 6859.697d, 54.68d, -15.6544d, 23.44d};
+        System.out.println("= 13997.3765/7");
+        for (double n: name){
+         if (n < 0);
+         break;
+            System.out.println(name[1]);
+
+
+
+        }
+
+    }
+
+    }
+
